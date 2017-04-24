43c796b5ebc646b9d66549a1acd06fc84fd43b24
cy <992131860@qq.com>
2017-04-24 23:54:55 +0800

    comm

--- api/controllers/TransferController.js
+++ api/controllers/TransferController.js
@@ -482,7 +482,7 @@ module.exports = {
     //基础的温湿度
     //连接数据库
     // settings.db = {
-    //   host: '127.0.0.1:1337',
+    //   host: '116.62.28.28:1337',
     //   user: 'root',
     //    //modify
     //   password: 'admin123',
